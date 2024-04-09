b5f7d1747ac7877767fb0563d649d8ab42aa2d14
Sadgman <angel.luis.regalado@hotmail.com>
2024-04-09 10:11:21 -0400

    Agregada función newIndexP para evitar preguntas repetidas

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const fs = require("fs");
 
 const question = fs.readFileSync(__dirname + "/questions.json", "utf-8");
 let data_question = JSON.parse(question);
-const index_p = Math.floor(Math.random() * Object.keys(data_question["p"]).length) + 1;
+let ListOfQuestResp = [];
+let index_p = Math.floor(Math.random() * Object.keys(data_question["p"]).length) + 1;
+
+
+function newIndexP(){
+    do{
+        index_p = Math.floor(Math.random() * Object.keys(data_question["p"]).length) + 1;
+    }while(ListOfQuestResp.slice(-4).includes(index_p));
+    
+    ListOfQuestResp.push(index_p);
+    
+    if(ListOfQuestResp.length === 5){
+        ListOfQuestResp = [];
+    }
+}
 
 function readTitle(){
     return data_question["p"][index_p];
@@ -32,4 +46,4 @@ function correctAnswerIndex(){
 function correctAnswer(){
     return data_question["r"][index_p]["correct"];
 }
-module.exports = {readTitle, readResponse, correctAnswer, correctAnswerIndex};
+module.exports = {readTitle, readResponse, correctAnswer, correctAnswerIndex, newIndexP};
