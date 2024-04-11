6089933c3736cdc1513a17e7d971af07943f0e39
Sadgman <angel.luis.regalado@hotmail.com>
2024-04-11 11:26:13 -0400

    Agregada función isCorrect para verificar si una respuesta es correcta

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ let data_question = JSON.parse(question);
 let ListOfQuestResp = [];
 let index_p = Math.floor(Math.random() * Object.keys(data_question["p"]).length) + 1;
 
-
 function newIndexP(){
+    /**
+     * Esta función crea un nuevo indice comprobando si el indice ya ha sido usado anteriormente en la lista de preguntas y si no es así guarda el  indice en la lista, actualiza index_p.
+     */
     do{
         index_p = Math.floor(Math.random() * Object.keys(data_question["p"]).length) + 1;
     }while(ListOfQuestResp.slice(-4).includes(index_p));
@@ -43,7 +45,26 @@ function correctAnswerIndex(){
         }
     }
 }
+function isCorrect(resp){
+/**
+ * @param {string} resp
+ * @return {boolean}
+ */
+    if(typeof resp === "string"){
+        if(resp === data_question["r"][index_p]["correct"]){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(typeof resp === "number"){
+        if(resp === correctAnswerIndex()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
 function correctAnswer(){
     return data_question["r"][index_p]["correct"];
 }
-module.exports = {readTitle, readResponse, correctAnswer, correctAnswerIndex, newIndexP};
+module.exports = {readTitle, readResponse, correctAnswer, correctAnswerIndex, newIndexP, isCorrect};
